e8a3d4c3e85fa47255db6c3dc1f8072d1f4ed3b8
ext.yujiaming <ext.yujiaming@jd.com>
2022-04-29 17:16:40 +0800

    three

--- 10-3d/three/demo1/src/index.js
+++
@@ -1,5 +0,0 @@
-import * as THREE from '../libs/Three.js'
-
-const scene = new THREE.Scene();
-console.log("🚀 ~ file: index.js ~ line 3 ~ scene", scene)
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
